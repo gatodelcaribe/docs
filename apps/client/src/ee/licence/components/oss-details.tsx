72888dfc92889e6086c2530af41e8adad05421ad
Gato del Caribe <144469623+gatodelcaribe@users.noreply.github.com>
2025-06-10 20:15:45 +0200

    Update branding

--- apps/client/src/ee/licence/components/oss-details.tsx
+++ apps/client/src/ee/licence/components/oss-details.tsx
@@ -11,7 +11,7 @@ export default function OssDetails() {
         withTableBorder
       >
         <Table.Caption>
-          To unlock enterprise features like SSO, contact sales@docmost.com.
+          Happy Demonia
         </Table.Caption>
         <Table.Tbody>
           <Table.Tr>
